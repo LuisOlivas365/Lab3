984e677e15d045a05c7c7dd14a86b042625ce702
Luis OM <marrufoluis9@gmail.com>
2022-10-03 22:05:32 -0600

    Repo Lab3

---
+++ main.cpp
@@ -0,0 +1,143 @@
+ // FileName:        main.c
+ // Program version: Dev-C++ 6.3
+ // Company:         TECNM - ITCH
+ // Description:     ESTRUCTURAS DE DATOS.
+ // Authors:         LUIS ALEJANDRO OLIVAS MARRUFO 19060735
+ // Updated:         10/2022
+ //Nota:  Convertidor ADC
+#include <iostream>
+#include<stdlib.h> 
+#include<stdio.h>
+using namespace std; 
+
+class ConvertidorADC{
+	
+	private:   //Atributos
+		
+		int res,ncanales,frec,*canales;   //resolucion,no. de canales,frescuencia de muestreo, vector de memoria dinamica donde se almacenan numeros de canales
+		float *voltaje,*resolucion_salida;//vectores de memoria dinamica donde se almacenara el voltaje que leerá el convertidor,y la salida del convertidor
+	
+	public: 
+	
+		ConvertidorADC(int,int,int);  //no. de canales,resolucion de los canales,frecuencia de muestreo,canales
+		void setncanales(int _ncanales,int _res,int _frec);
+		void calculador();
+}; 
+
+/********************Metodos*********************************/ 
+
+ConvertidorADC::ConvertidorADC(int _ncanales=0,int _res=0,int _frec=0): ncanales(_ncanales),res(_res),frec(_frec)//Constructor
+{
+  canales= new int[ncanales]; 
+  voltaje= new float[ncanales];
+  resolucion_salida= new float[ncanales];
+}  
+
+void ConvertidorADC::calculador(){
+	
+		for(int i=0; i<ncanales; i++){
+		cout<<"Seleccione el canal a usar (max 32): AN";
+		cin>>canales[i];
+	}   
+	
+		for(int i=0; i<ncanales; i++){
+		
+			int resl;
+			cout<<"Ingresa el voltaje (max 3.3v) del AN"<<canales[i]<<":";
+			cin>> voltaje[i];
+			
+			switch(res)
+			{
+				case 8:
+					 resl=255; 
+					 resolucion_salida[i]=(voltaje[i]/3.3)*resl;
+					break; 
+					
+				case 10:
+					 resl=1023;
+					 resolucion_salida[i]=(voltaje[i]/3.3)*resl;
+					break;
+				
+				case 12: 
+					 resl=4095;
+					 resolucion_salida[i]=(voltaje[i]/3.3)*resl;
+					break;
+					
+				default: break;
+			}
+	} 
+	
+	cout<<"\nMostrando datos";
+	cout<<"\nResolucion "<<res<<" bits "<<endl;
+	cout<<"\nFrecuencia "<<frec<<"Khz "<<endl;
+	
+	for(int i=0; i<ncanales; i++){ 
+	    
+		cout<<"Canal AN"<<canales[i]<<endl; 
+		cout<<"Voltaje de entrada: "<<voltaje[i]<<"V"<<endl;
+		cout<<"Dato digital: "<<resolucion_salida[i]<<endl; 
+	}
+	
+	delete[] canales; 
+	delete[] voltaje;
+	delete[] resolucion_salida;
+}
+
+///Setter
+void ConvertidorADC::setncanales(int _ncanales,int _res,int _frec){
+  ncanales=_ncanales; 
+  res=_res; 
+  frec=_frec;
+}
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) { 
+	
+	int validador,ncanales,res,frec;
+	
+    do{
+    	
+		cout<<"Ingrese resolucion (8,10,12)"<<endl;
+    	if(scanf("%i", &res) != 1){
+    		cout<<"Dato no válido"<<endl; 
+	        validador=1;
+		}
+		
+		else
+		validador=0;
+		
+	}while(validador==1); 
+	
+	    do{
+    	
+		cout<<"Ingrese  frecuencia"<<endl;
+    	if(scanf("%i", &frec) != 1){
+    		cout<<"Dato no válido"<<endl; 
+	        validador=1;	
+		}
+		
+		else
+		validador=0;
+		
+	}while(validador==1);   
+	
+		do{
+    	
+		cout<<"Ingrese no. de canales"<<endl;
+    	if(scanf("%i", &ncanales) != 1){
+    		cout<<"Dato no válido"<<endl; 
+	        validador=1;	
+		}
+		else
+		validador=0;
+		
+	}while(validador==1);  
+	
+	ConvertidorADC convertidor; //Se crea el objeto convertidor 
+	convertidor.setncanales(ncanales,res,frec); 
+	convertidor.calculador();
+	
+	return 0;
+}
